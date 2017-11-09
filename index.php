e1fc63dcec959bd11c21fb849ecd4a471e96370f
Lucien-CESI <lucien.brouillaud@wanadoo.fr>
2017-11-09 14:24:33 +0100

    Projet CESI - Version 0.1a
    Le Working Directory du projet stagiaire du CESI, réalisé par Valentin et Lucien.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/index.css">
+        <title>Accueil</title>
+    </head>
+    <body>
+		<div class ="container">
+            <div class="row justify-content-center">
+	            <div class="col-10 col-md-6 col-lg-5 text-center popup">
+                    <p class="titre">Veuillez prouver votre appartenance au CESI :</p>
+	                <form method = "post" action = "traitement.php">
+						<label for ="identifiant"> Identifiant : </label>
+                        <input type="text" class="inputte col-6" name="identifiant" placeholder="Ex: V@lentin" id="identifiant">
+
+						<label for="pass"> Mot de passe : </label>
+                        <input type="password" class = "col-6 inputte" name="pass" id="pass">
+
+						<input type="submit" name="connexion" value ="S'identifier">
+	                </form>
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="lib/js/jquery.min.js"></script>
+        <script src="lib/js/popper.min.js"></script>
+        <script src="lib/js/bootstrap.min.js"></script>
+    </body>
+</html>
